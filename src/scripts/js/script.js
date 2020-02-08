88420c06f34b3e345c93c0eb47b201d59c585398
olivermirimu <olivermirimu@gamil.com>
2020-02-08 09:26:39 +0300

    new commit

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -1,11 +1,12 @@
-//Service Worker registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./../serviceWorker.js')
-      .then(reg => {
-        console.log('Succesfully registered: ', reg);
-      }).catch(err => {
-        console.log('Sorry nimekataa: ', err);
-      });
-  });
-}
+// //Service Worker registration
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('./../serviceWorker.js', {
+//       scope: '/src/'
+//     }).then(reg => {
+//       console.log('Succesfully registered: ', reg);
+//     }).catch(err => {
+//       console.log('Sorry nimekataa: ', err);
+//     });
+//   });
+// }
